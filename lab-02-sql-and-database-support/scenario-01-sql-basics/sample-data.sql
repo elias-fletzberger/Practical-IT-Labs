69b1660f4b6307e15d395215e2c4b6f357246141
Elias <elias-fletzberger@web.de>
2026-02-12 22:47:46 +0100

    Add files via upload

---
+++ lab-02-sql-and-database-support/scenario-01-sql-basics/sample-data.sql
@@ -0,0 +1,33 @@
+INSERT INTO stores (name, city)
+VALUES
+	('Filiale Hamburg', 'Hamburg'),
+	('Filiale Berlin', 'Berlin'),
+	('Filiale München', 'München');
+
+
+INSERT INTO products (name, price)
+VALUES
+	('Cola 0.5L', 1.49),
+	('Mineralwasser 0.5L', 0.89),
+	('Apfelschorle 0.5L', 1.29),
+	('Eistee 0.5L', 1.19);
+
+
+INSERT INTO sales (store_id, product_id, quantity, sale_date)
+VALUES
+	(1, 1, 120, '2026-02-09'),
+	(1, 2, 200, '2026-02-09'),
+	(2, 3, 90,  '2026-02-13'),
+	(2, 4, 150, '2026-02-12'),
+	(3, 1, 80,  '2026-02-11'),
+	(3, 2, 170, '2026-02-12'),
+	(1, 3, 140, '2026-02-10'),
+	(1, 4, 210, '2026-02-11'),
+	(2, 1, 92,  '2026-02-13'),
+	(2, 2, 120, '2026-02-09'),
+	(3, 3, 85,  '2026-02-09'),
+	(3, 4, 17,  '2026-02-10'),
+	(1, 1, 120, '2100-02-09'),
+	(1, 1, -5,  '2026-02-08');
+
+	
