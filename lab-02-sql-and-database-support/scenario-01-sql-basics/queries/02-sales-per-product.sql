639afa6435aa47b44e0c331b67724246d1d3fbcf
Elias <elias-fletzberger@web.de>
2026-02-12 23:22:26 +0100

    Delete lab-02-sql-and-database-support/scenario-01-sql-basics/queries directory

--- lab-02-sql-and-database-support/scenario-01-sql-basics/queries/02-sales-per-product.sql
+++
@@ -1,8 +0,0 @@
---Umsatz pro Produkt
-SELECT
-	pr.name AS product_name,
-	SUM(sa.quantity * pr.price) AS product_sales
-FROM sales sa
-JOIN products pr ON sa.product_id = pr.id
-GROUP BY pr.name, pr.id
-ORDER BY product_sales DESC;
--- lab-02-sql-and-database-support/scenario-01-sql-basics/queries/05-quantity-validation.sql
+++
@@ -1,6 +0,0 @@
---Plausibilitätscheck (reale Menge)
-SELECT
-    id,
-    quantity
-FROM sales
-WHERE quantity <= 0;
